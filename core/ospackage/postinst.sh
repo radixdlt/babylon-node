8e3a9df0513f578ef9ac9d541e7867e4e3510747
David Edey <david@rdx.works>
2022-04-28 15:03:11 +0100

    Various gradle and docker fixes

--- core/ospackage/postinst.sh
+++ core/ospackage/postinst.sh
@@ -2,22 +2,29 @@
 
 set -ex
 
+# NB - If updating this script, also update ./docker/scripts/config_radixdlt.sh
+
 # vars
 RADIXDLT_USER=radixdlt
-RADIXDLT_HOME=/opt/$RADIXDLT_USER
+RADIXDLT_SERVICE_FILE=radixdlt.service
+RADIXDLT_DIRECTORY_NAME=radixdlt
+
+# paths
+RADIXDLT_HOME="/opt/$RADIXDLT_DIRECTORY_NAME"
+RADIXDLT_LOG_DIR="/var/log/$RADIXDLT_DIRECTORY_NAME"
 
 # create user and group idempotently
 getent group $RADIXDLT_USER >/dev/null || groupadd -r $RADIXDLT_USER
 getent passwd $RADIXDLT_USER >/dev/null || useradd -r -d "$RADIXDLT_HOME" -g $RADIXDLT_USER $RADIXDLT_USER
 
 # create log dir
-mkdir -p /var/log/$RADIXDLT_USER
+mkdir -p "$RADIXDLT_LOG_DIR"
 
 # make sure all files are owned by the radixdlt user/group
-chown -Rf "$RADIXDLT_USER:$RADIXDLT_USER" "$RADIXDLT_HOME" /var/log/$RADIXDLT_USER
+chown -Rf "$RADIXDLT_USER:$RADIXDLT_USER" "$RADIXDLT_HOME" "$RADIXDLT_LOG_DIR"
 
 # Make sure that systemd files are owned by root
-chown root:root /etc/systemd/system/$RADIXDLT_USER.service
+chown root:root "/etc/systemd/system/$RADIXDLT_SERVICE_FILE"
 
 #systemctl daemon-reload
-#systemctl start $RADIXDLT_USER.service
+#systemctl start $RADIXDLT_SERVICE_FILE
--- docker/scripts/config_radixdlt.sh
+++ docker/scripts/config_radixdlt.sh
@@ -2,19 +2,19 @@
 
 set -ex
 
-# need this to run on Alpine (grsec) kernels without crashing
+# Need this to run on Alpine (grsec) kernels without crashing
 find /usr -type f -name java -exec setfattr -n user.pax.flags -v em {} \;
 
-#Add user using env variable if provided
+# Add user using env variable if provided
 USER_ID=${LOCAL_USER_ID:-999}
 USER_NAME=radixdlt
-# check and delete the user that is created in postinstal action of deb package
+# Check and delete the user that is created in postinstall action of deb package
 getent group $USER_NAME >/dev/null && groupmod -g $USER_ID radixdlt || groupadd -r $USER_NAME -g $USER_ID
 getent passwd $USER_NAME >/dev/null && usermod -u $USER_ID radixdlt || useradd -r -d "$RADIXDLT_HOME" -g $USER_NAME $USER_NAME
 chown -R radixdlt:radixdlt /home/radixdlt/
-chmod u=xr /opt/radixdlt/bin/radixdlt
+chmod u=xr /opt/radixdlt/bin/core
 
-#check for test network configs
+# Check for test network configs
 TEST_CONFIGS="${RADIXDLT_HOME:?}"/test.config
 if test -f "$TEST_CONFIGS"; then
     echo >> default.config.envsubst
