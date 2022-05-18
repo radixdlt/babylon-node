ab67d46211634e148d1845745710e4d4aa799bfa
Gianluca Guida <gianluca@rdx.works>
2022-05-18 16:23:22 -0400

    Add simple JNI call between mempool and rust.

---
+++ state-manager/src/main/rust/stateman/src/lib.rs
@@ -0,0 +1,30 @@
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+use jni::JNIEnv;
+
+#[no_mangle]
+pub extern "system" fn Java_com_radixdlt_mempool_REv2Mempool_hello(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+) -> jstring {
+    let input: String = env
+        .get_string(input)
+        .expect("Couldn't get java string!")
+        .into();
+
+    let output = env
+        .new_string(format!("Hello, {}!", input))
+        .expect("Couldn't create java string!");
+
+    output.into_inner()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
