bc6dd137a93fc9bfaf9779bf6df03c7554f5e170
Gianluca Guida <gianluca@rdx.works>
2022-05-26 19:55:11 -0400

    state-manager: Add mempool, use traits for state manager interface

--- state-manager/src/main/rust/stateman/src/jni/state_manager.rs
+++ state-manager/src/main/rust/stateman/src/jni/state_manager.rs
@@ -13,11 +13,7 @@ extern "system" fn Java_com_radixdlt_statemanager_StateManager_init(
     interop_state: JObject,
     j_public_key: jbyteArray,
 ) {
-    let public_key: Vec<u8> = env.convert_byte_array(j_public_key).unwrap();
-    let state_manager = Arc::new(StateManager::new(public_key));
-
-    env.set_rust_field(interop_state, JNI_FIELD_NAME, state_manager)
-        .unwrap();
+    JNIStateManager::init(env, interop_state, j_public_key);
 }
 
 #[no_mangle]
@@ -26,13 +22,43 @@ extern "system" fn Java_com_radixdlt_statemanager_StateManager_cleanup(
     _class: JClass,
     interop_state: JObject,
 ) {
-    let state_manager: Arc<StateManager> =
-        env.take_rust_field(interop_state, JNI_FIELD_NAME).unwrap();
-
-    drop(state_manager);
+    JNIStateManager::cleanup(env, interop_state);
 }
 
-pub fn jni_state_manager(env: JNIEnv, interop_state: JObject) -> Arc<StateManager> {
-    let sm: &Arc<StateManager> = &env.get_rust_field(interop_state, JNI_FIELD_NAME).unwrap();
-    Arc::clone(sm)
+use crate::mempool::mock::MockMempool;
+use crate::transaction_store::TransactionStore;
+use crate::vertex_store::VertexStore;
+
+pub struct JNIStateManager {
+    state_manager: Arc<StateManager<MockMempool>>,
+}
+
+impl JNIStateManager {
+    pub fn init(env: JNIEnv, interop_state: JObject, j_public_key: jbyteArray) {
+        // Build the basic subcomponents.
+        let mempool = MockMempool::new(64); // TODO: parameter
+        let vtxstore = VertexStore::new();
+        let txnstore = TransactionStore::new();
+
+        // Build the state manager.
+        let state_manager = Arc::new(StateManager::new(mempool, vtxstore, txnstore));
+
+        let nodesm = JNIStateManager { state_manager };
+
+        env.set_rust_field(interop_state, JNI_FIELD_NAME, nodesm)
+            .unwrap();
+    }
+
+    pub fn cleanup(env: JNIEnv, interop_state: JObject) {
+        let nodesm: JNIStateManager = env.take_rust_field(interop_state, JNI_FIELD_NAME).unwrap();
+        drop(nodesm);
+    }
+
+    pub fn get_state_manager(
+        env: JNIEnv,
+        interop_state: JObject,
+    ) -> Arc<StateManager<MockMempool>> {
+        let nodesm: &JNIStateManager = &env.get_rust_field(interop_state, JNI_FIELD_NAME).unwrap();
+        Arc::clone(&nodesm.state_manager)
+    }
 }
--- state-manager/src/main/rust/stateman/src/state_manager.rs
+++ state-manager/src/main/rust/stateman/src/state_manager.rs
@@ -1,21 +1,28 @@
+use crate::mempool::Mempool;
 use crate::transaction_store::TransactionStore;
 use crate::vertex_store::VertexStore;
 use std::sync::Arc;
 use std::sync::Mutex;
 
 #[derive(Clone, Debug)]
-pub struct StateManager {
+pub struct StateManager<M: Mempool> {
     pub public_key: Vec<u8>,
+    pub mempool: Arc<Mutex<M>>,
     pub vertex_store: Arc<Mutex<VertexStore>>,
     pub transaction_store: Arc<Mutex<TransactionStore>>,
 }
 
-impl StateManager {
-    pub fn new(public_key: Vec<u8>) -> StateManager {
+impl<M: Mempool> StateManager<M> {
+    pub fn new(
+        mempool: M,
+        vertex_store: VertexStore,
+        transaction_store: TransactionStore,
+    ) -> StateManager<M> {
         StateManager {
-            public_key,
-            vertex_store: Arc::new(Mutex::new(VertexStore::new())),
-            transaction_store: Arc::new(Mutex::new(TransactionStore::new())),
+            public_key: Vec::new(),
+            mempool: Arc::new(Mutex::new(mempool)),
+            vertex_store: Arc::new(Mutex::new(vertex_store)),
+            transaction_store: Arc::new(Mutex::new(transaction_store)),
         }
     }
 }
