d4289ec52942f0d03ad32ed3beac6e722cb5e327
David Edey <david@rdx.works>
2022-06-10 03:29:16 +0100

    Support for enum sbor encoding

---
+++ common/src/main/java/com/radixdlt/sbor/codec/EnumCodec.java
@@ -0,0 +1,158 @@
+/* Copyright 2021 Radix Publishing Ltd incorporated in Jersey (Channel Islands).
+ *
+ * Licensed under the Radix License, Version 1.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at:
+ *
+ * radixfoundation.org/licenses/LICENSE-v1
+ *
+ * The Licensor hereby grants permission for the Canonical version of the Work to be
+ * published, distributed and used under or by reference to the Licensor’s trademark
+ * Radix ® and use of any unregistered trade names, logos or get-up.
+ *
+ * The Licensor provides the Work (and each Contributor provides its Contributions) on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * including, without limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Whilst the Work is capable of being deployed, used and adopted (instantiated) to create
+ * a distributed ledger it is your responsibility to test and validate the code, together
+ * with all logic and performance of that code under all foreseeable scenarios.
+ *
+ * The Licensor does not make or purport to make and hereby excludes liability for all
+ * and any representation, warranty or undertaking in any form whatsoever, whether express
+ * or implied, to any entity or person, including any representation, warranty or
+ * undertaking, as to the functionality security use, value or other characteristics of
+ * any distributed ledger nor in respect the functioning or value of any tokens which may
+ * be created stored or transferred using the Work. The Licensor does not warrant that the
+ * Work or any use of the Work complies with any law or regulation in any territory where
+ * it may be implemented or used or that it will be appropriate for any specific purpose.
+ *
+ * Neither the licensor nor any current or former employees, officers, directors, partners,
+ * trustees, representatives, agents, advisors, contractors, or volunteers of the Licensor
+ * shall be liable for any direct or indirect, special, incidental, consequential or other
+ * losses of any kind, in tort, contract or otherwise (including but not limited to loss
+ * of revenue, income or profits, or loss of use or data, or loss of reputation, or loss
+ * of any economic or other opportunity of whatsoever nature or howsoever arising), arising
+ * out of or in connection with (without limitation of any use, misuse, of any ledger system
+ * or use made or its functionality or any performance or operation of any code or protocol
+ * caused by bugs or programming or logic errors or otherwise);
+ *
+ * A. any offer, purchase, holding, use, sale, exchange or transmission of any
+ * cryptographic keys, tokens or assets created, exchanged, stored or arising from any
+ * interaction with the Work;
+ *
+ * B. any failure in a transmission or loss of any token or assets keys or other digital
+ * artefacts due to errors in transmission;
+ *
+ * C. bugs, hacks, logic errors or faults in the Work or any communication;
+ *
+ * D. system software or apparatus including but not limited to losses caused by errors
+ * in holding or transmitting tokens by any third-party;
+ *
+ * E. breaches or failure of security including hacker attacks, loss or disclosure of
+ * password, loss of private key, unauthorised use or misuse of such passwords or keys;
+ *
+ * F. any losses including loss of anticipated savings or other benefits resulting from
+ * use of the Work or any changes to the Work (however implemented).
+ *
+ * You are solely responsible for; testing, validating and evaluation of all operation
+ * logic, functionality, security and appropriateness of using the Work for any commercial
+ * or non-commercial purpose and for any reproduction or redistribution by You of the
+ * Work. You assume all risks associated with Your use of the Work and the exercise of
+ * permissions under this License.
+ */
+
+package com.radixdlt.sbor.codec;
+
+import com.radixdlt.lang.Functions;
+import com.radixdlt.sbor.codec.constants.TypeId;
+import com.radixdlt.sbor.coding.DecoderApi;
+import com.radixdlt.sbor.coding.EncoderApi;
+import com.radixdlt.sbor.exceptions.SborCodecException;
+import com.radixdlt.sbor.exceptions.SborDecodeException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnumCodec<T> implements Codec<T> {
+  private final Map<String, FieldsCodec<?>> entries;
+  private final Functions.Func1<T, String> mapEnumValueToKey;
+
+  public EnumCodec(
+      Map<String, FieldsCodec<?>> entries, Functions.Func1<T, String> mapEnumValueToKey) {
+    this.entries = entries;
+    this.mapEnumValueToKey = mapEnumValueToKey;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void encode(EncoderApi encoder, T value) {
+    encoder.encodeTypeId(TypeId.TYPE_ENUM);
+
+    var enumKey = mapEnumValueToKey.apply(value);
+
+    var enumEntry = entries.get(enumKey);
+
+    if (enumEntry == null) {
+      throw new SborCodecException(
+          String.format("Enum entry enumKey %s not found in entries map", enumKey));
+    }
+
+    encoder.writeString(enumKey);
+    ((FieldsCodec<T>) enumEntry).encode(encoder, value);
+  }
+
+  @SuppressWarnings("unchecked")
+  public T decode(DecoderApi decoder) {
+    decoder.expectType(TypeId.TYPE_ENUM);
+    var enumKey = decoder.readString();
+
+    var enumEntry = entries.get(enumKey);
+
+    if (enumEntry == null) {
+      throw new SborDecodeException(
+          String.format("Enum entry enumKey %s not found in entries map", enumKey));
+    }
+
+    return ((FieldsCodec<T>) enumEntry).decode(decoder);
+  }
+
+  /**
+   * An Enum Codec for Abstract Data Types (ie sealed interfaces with distinct subclasses). It
+   * assumes each FieldsCodec entry relates to a distinct subclass, and that the names of the
+   * subclasses are the enum key names to use in serialization.
+   *
+   * @param <T> The interface type, of which each FieldsCodec is a subclass
+   */
+  public static <T> EnumCodec<T> fromEntries(FieldsCodec<?>... enumEntries) {
+    var fieldsCodecMap = new HashMap<String, FieldsCodec<?>>();
+    var inverseClassMap = new HashMap<Class<?>, String>();
+    for (var fieldsCodec : enumEntries) {
+      var baseClass = fieldsCodec.getBaseClass();
+      var enumKey = baseClass.getSimpleName();
+      fieldsCodecMap.put(enumKey, fieldsCodec);
+      inverseClassMap.put(baseClass, enumKey);
+    }
+
+    if (inverseClassMap.size() < enumEntries.length) {
+      throw new SborCodecException(
+          "Some field codecs are for the same class - this isn't supported with the fromEntries"
+              + " builder");
+    }
+
+    if (fieldsCodecMap.size() < enumEntries.length) {
+      throw new SborCodecException(
+          "Some field codecs are for classes which have the same simple name - this isn't supported"
+              + " with the fromEntries builder");
+    }
+
+    return new EnumCodec<>(
+        fieldsCodecMap,
+        enumEntry -> {
+          var key = inverseClassMap.get(enumEntry.getClass());
+          if (key == null) {
+            throw new SborCodecException(
+                String.format("Class %s not found in inverseClassMap", enumEntry.getClass()));
+          }
+          return key;
+        });
+  }
+}
--- common/src/test/java/com/radixdlt/sbor/SborCoderTest.java
+++ common/src/test/java/com/radixdlt/sbor/SborCoderTest.java
@@ -64,74 +64,75 @@
 
 package com.radixdlt.sbor;
 
+import static com.radixdlt.lang.Option.none;
+import static com.radixdlt.lang.Option.some;
+import static org.junit.Assert.*;
+
 import com.google.common.reflect.TypeToken;
 import com.radixdlt.lang.Either;
 import com.radixdlt.lang.Option;
 import com.radixdlt.lang.Unit;
+import com.radixdlt.sbor.dto.SimpleEnum;
 import com.radixdlt.sbor.dto.SimpleRecord;
 import org.junit.Test;
-
-import static com.radixdlt.lang.Option.none;
-import static com.radixdlt.lang.Option.some;
-import static org.junit.Assert.*;
 
 public class SborCoderTest {
   @Test
   public void unitCanBeEncodedAndDecoded() {
 
-    var r0 = SborCoder.DEFAULT.encode(Unit.unit(), Unit.class);
+    var r0 = Sbor.encode(Unit.unit(), Unit.class);
 
     assertEquals(1, r0.length);
     assertEquals(0, r0[0]); // Type == 0x00 - Unit
 
-    var r1 = SborCoder.DEFAULT.decode(r0, Unit.class);
+    var r1 = Sbor.decode(r0, Unit.class);
 
     assertEquals(Unit.unit(), r1);
   }
 
   @Test
   public void booleanCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode(true, boolean.class);
+    var r0 = Sbor.encode(true, boolean.class);
 
     assertEquals(2, r0.length);
     assertEquals(1, r0[0]); // Type == 0x01 - Boolean
     assertEquals(1, r0[1]); // Value = 1 == true
 
-    var r1 = SborCoder.DEFAULT.decode(r0, boolean.class);
+    var r1 = Sbor.decode(r0, boolean.class);
 
     assertTrue(r1);
   }
 
   @Test
   public void byteCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode((byte) 0x5A, byte.class);
+    var r0 = Sbor.encode((byte) 0x5A, byte.class);
 
     assertEquals(2, r0.length);
     assertEquals(7, r0[0]); // Type == 0x07 - u8
     assertEquals(0x5A, r0[1]); // Value == 0x5A
 
-    var r1 = SborCoder.DEFAULT.decode(r0, byte.class);
+    var r1 = Sbor.decode(r0, byte.class);
 
     assertEquals((byte) 0x5A, (byte) r1);
   }
 
   @Test
   public void shortCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode((short) 0x1234, short.class);
+    var r0 = Sbor.encode((short) 0x1234, short.class);
 
     assertEquals(3, r0.length);
     assertEquals(3, r0[0]); // Type == 0x03 - i16
     assertEquals(0x34, r0[1]); // Value
     assertEquals(0x12, r0[2]); // Value
 
-    var r1 = SborCoder.DEFAULT.decode(r0, short.class);
+    var r1 = Sbor.decode(r0, short.class);
 
     assertEquals((short) 0x1234, (short) r1);
   }
 
   @Test
   public void intCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode(0x12345678, int.class);
+    var r0 = Sbor.encode(0x12345678, int.class);
 
     assertEquals(5, r0.length);
     assertEquals(4, r0[0]); // Type == 0x04 - i32
@@ -140,14 +141,14 @@ public class SborCoderTest {
     assertEquals(0x34, r0[3]); // Value
     assertEquals(0x12, r0[4]); // Value
 
-    var r1 = SborCoder.DEFAULT.decode(r0, int.class);
+    var r1 = Sbor.decode(r0, int.class);
 
     assertEquals(0x12345678, (int) r1);
   }
 
   @Test
   public void longCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode(0x0123_4567_89AB_CDEFL, long.class);
+    var r0 = Sbor.encode(0x0123_4567_89AB_CDEFL, long.class);
 
     assertEquals(9, r0.length);
     assertEquals(5, r0[0]); // Type == 0x05 - i64
@@ -160,14 +161,14 @@ public class SborCoderTest {
     assertEquals(0x23, r0[7]); // Value
     assertEquals(0x01, r0[8]); // Value
 
-    var r1 = SborCoder.DEFAULT.decode(r0, long.class);
+    var r1 = Sbor.decode(r0, long.class);
 
     assertEquals(0x0123_4567_89AB_CDEFL, (long) r1);
   }
 
   @Test
   public void stringCanBeEncodedAndDecoded() {
-    var r0 = SborCoder.DEFAULT.encode("Test string", String.class);
+    var r0 = Sbor.encode("Test string", String.class);
 
     assertEquals(16, r0.length);
     assertEquals(0x0C, r0[0]); // Type == 0x0C - String
@@ -187,7 +188,7 @@ public class SborCoderTest {
     assertEquals('n', r0[14]);
     assertEquals('g', r0[15]);
 
-    var r1 = SborCoder.DEFAULT.decode(r0, String.class);
+    var r1 = Sbor.decode(r0, String.class);
 
     assertEquals("Test string", r1);
   }
@@ -196,7 +197,7 @@ public class SborCoderTest {
   public void byteArrayCanBeEncodedAndDecoded() {
     var testVector = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05};
 
-    var r0 = SborCoder.DEFAULT.encode(testVector, byte[].class);
+    var r0 = Sbor.encode(testVector, byte[].class);
 
     assertEquals(11, r0.length);
     assertEquals(0x30, r0[0]); // Type == 0x30 - Vector
@@ -211,7 +212,7 @@ public class SborCoderTest {
     assertEquals(0x04, r0[9]);
     assertEquals(0x05, r0[10]);
 
-    var r1 = SborCoder.DEFAULT.decode(r0, byte[].class);
+    var r1 = Sbor.decode(r0, byte[].class);
 
     assertArrayEquals(testVector, r1);
   }
@@ -220,7 +221,7 @@ public class SborCoderTest {
   public void shortArrayCanBeEncodedAndDecoded() {
     var testVector = new short[] {0x0102, 0x0304};
 
-    var r0 = SborCoder.DEFAULT.encode(testVector, short[].class);
+    var r0 = Sbor.encode(testVector, short[].class);
 
     assertEquals(10, r0.length);
     assertEquals(0x30, r0[0]); // Type == 0x30 - Vector
@@ -234,7 +235,7 @@ public class SborCoderTest {
     assertEquals(0x04, r0[8]);
     assertEquals(0x03, r0[9]);
 
-    var r1 = SborCoder.DEFAULT.decode(r0, short[].class);
+    var r1 = Sbor.decode(r0, short[].class);
 
     assertArrayEquals(testVector, r1);
   }
@@ -243,7 +244,7 @@ public class SborCoderTest {
   public void intArrayCanBeEncodedAndDecoded() {
     var testVector = new int[] {0x01020304, 0x05060708, 0x090A0B0C};
 
-    var r0 = SborCoder.DEFAULT.encode(testVector, int[].class);
+    var r0 = Sbor.encode(testVector, int[].class);
 
     assertEquals(18, r0.length);
     assertEquals(0x30, r0[0]); // Type == 0x30 - Vector
@@ -265,7 +266,7 @@ public class SborCoderTest {
     assertEquals(0x0A, r0[16]);
     assertEquals(0x09, r0[17]);
 
-    var r1 = SborCoder.DEFAULT.decode(r0, int[].class);
+    var r1 = Sbor.decode(r0, int[].class);
 
     assertArrayEquals(testVector, r1);
   }
@@ -274,7 +275,7 @@ public class SborCoderTest {
   public void longArrayCanBeEncodedAndDecoded() {
     var testVector = new long[] {0x0102030405060708L, 0x090A0B0C11121314L};
 
-    var r0 = SborCoder.DEFAULT.encode(testVector);
+    var r0 = Sbor.encode(testVector);
 
     assertEquals(22, r0.length);
     assertEquals(0x30, r0[0]); // Type == 0x30 - Vector
@@ -300,7 +301,7 @@ public class SborCoderTest {
     assertEquals(0x0A, r0[20]);
     assertEquals(0x09, r0[21]);
 
-    var r1 = SborCoder.DEFAULT.decode(r0, long[].class);
+    var r1 = Sbor.decode(r0, long[].class);
 
     assertArrayEquals(testVector, r1);
   }
@@ -308,14 +309,14 @@ public class SborCoderTest {
   @Test
   public void someOptionCanBeEncodedAndDecoded() {
     var optionTypeLiteral = new TypeToken<Option<String>>() {};
-    var r0 = SborCoder.DEFAULT.encode(some("Test value"), optionTypeLiteral);
+    var r0 = Sbor.encode(some("Test value"), optionTypeLiteral);
 
     assertEquals(17, r0.length);
     assertEquals(0x20, r0[0]); // Type == 0x20 - Option
     assertEquals(0x01, r0[1]); // Value - present
     assertEquals(0x0C, r0[2]); // Stored type - 0x0C - String
 
-    var r1 = SborCoder.DEFAULT.decode(r0, optionTypeLiteral);
+    var r1 = Sbor.decode(r0, optionTypeLiteral);
 
     assertEquals(some("Test value"), r1);
   }
@@ -323,13 +324,13 @@ public class SborCoderTest {
   @Test
   public void noneOptionCanBeEncodedAndDecoded() {
     var optionTypeLiteral = new TypeToken<Option<String>>() {};
-    var r0 = SborCoder.DEFAULT.encode(none(), optionTypeLiteral);
+    var r0 = Sbor.encode(none(), optionTypeLiteral);
 
     assertEquals(2, r0.length);
     assertEquals(0x20, r0[0]); // Type == 0x20 - Option
     assertEquals(0x00, r0[1]); // Value - missing
 
-    var r1 = SborCoder.DEFAULT.decode(r0, optionTypeLiteral);
+    var r1 = Sbor.decode(r0, optionTypeLiteral);
 
     assertEquals(none(), r1);
   }
@@ -339,22 +340,22 @@ public class SborCoderTest {
     var eitherTypeLiteral = new TypeToken<Either<String, Long>>() {};
 
     var leftValue = Either.<String, Long>left("Some value");
-    var leftEncoded = SborCoder.DEFAULT.encode(leftValue, eitherTypeLiteral);
+    var leftEncoded = Sbor.encode(leftValue, eitherTypeLiteral);
 
     assertEquals(17, leftEncoded.length);
-    assertEquals(0x24, leftEncoded[0]); // Type == 0x24 - Either
-    assertEquals(0x01, leftEncoded[1]); // Value - left
+    assertEquals(0x24, leftEncoded[0]); // Type == 0x24 - Result
+    assertEquals(0x01, leftEncoded[1]); // Value - Failure
     assertEquals(0x0C, leftEncoded[2]); // Value type - String
 
     var rightValue = Either.<String, Long>right(123L);
-    var rightEncoded = SborCoder.DEFAULT.encode(rightValue, eitherTypeLiteral);
+    var rightEncoded = Sbor.encode(rightValue, eitherTypeLiteral);
     assertEquals(11, rightEncoded.length);
-    assertEquals(0x24, rightEncoded[0]); // Type == 0x24 - Either
-    assertEquals(0x00, rightEncoded[1]); // Value - right
+    assertEquals(0x24, rightEncoded[0]); // Type == 0x24 - Result
+    assertEquals(0x00, rightEncoded[1]); // Value - Success
     assertEquals(0x05, rightEncoded[2]); // Value type - i64
 
-    var leftOut = SborCoder.DEFAULT.decode(leftEncoded, eitherTypeLiteral);
-    var rightOut = SborCoder.DEFAULT.decode(rightEncoded, eitherTypeLiteral);
+    var leftOut = Sbor.decode(leftEncoded, eitherTypeLiteral);
+    var rightOut = Sbor.decode(rightEncoded, eitherTypeLiteral);
 
     assertEquals(leftValue, leftOut);
     assertEquals(rightValue, rightOut);
@@ -364,7 +365,7 @@ public class SborCoderTest {
   public void objectTreeCanBeEncodedAndDecoded() {
     var testValue = new SimpleRecord(1234567, "Some string", Either.left(4567L), some(false));
 
-    var r0 = SborCoder.DEFAULT.encode(testValue, SimpleRecord.class);
+    var r0 = Sbor.encode(testValue, SimpleRecord.class);
 
     assertEquals(41, r0.length);
     assertEquals(0x10, r0[0]); // Type == 0x10 - Struct
@@ -373,8 +374,66 @@ public class SborCoderTest {
     assertEquals(0x00, r0[3]); //
     assertEquals(0x00, r0[4]); //
 
-    var r1 = SborCoder.DEFAULT.decode(r0, SimpleRecord.class);
+    var r1 = Sbor.decode(r0, SimpleRecord.class);
 
     assertEquals(testValue, r1);
+  }
+
+  @Test
+  public void enumCanBeEncodedAndDecoded() {
+    var enumOne = new SimpleEnum.A(4, "C");
+
+    var encodedEnumOne = Sbor.encode(enumOne, SimpleEnum.class);
+
+    assertArrayEquals(
+        new byte[] {
+          17, // Enum Type
+          1, 0, 0, 0, // String length 1
+          65, // "A"
+          2, 0, 0, 0, // Number of fields
+          4, // Field 1 - Int Type
+          4, 0, 0, 0, // Field 1 value
+          0x0c, // Field 2 - String Type
+          1, 0, 0, 0, // String length 1
+          67, // "C"
+        },
+        encodedEnumOne);
+
+    var decodedEnumOne = Sbor.decode(encodedEnumOne, SimpleEnum.class);
+
+    assertEquals(enumOne, decodedEnumOne);
+
+    var enumTwo = new SimpleEnum.B(Either.left(5L));
+    var encodedEnumTwo = Sbor.encode(enumTwo, SimpleEnum.class);
+
+    assertArrayEquals(
+        new byte[] {
+          17, // Enum Type
+          1,
+          0,
+          0,
+          0, // String length 1
+          66, // "B"
+          1,
+          0,
+          0,
+          0, // number of fields
+          0x24, // Field 1 - Either Type
+          0x01, // Field 1 - Left subtype
+          5, // Field 1 - Either left is of long type
+          5,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0 // Field 1 - long value
+        },
+        encodedEnumTwo);
+
+    var decodedEnumTwo = Sbor.decode(encodedEnumTwo, SimpleEnum.class);
+
+    assertEquals(enumTwo, decodedEnumTwo);
   }
 }
