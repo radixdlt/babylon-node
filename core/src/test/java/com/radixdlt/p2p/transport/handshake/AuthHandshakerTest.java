ea400207b3857924f94f04a1a5cf706b4c24266e
Les Sheppard <les.sheppard@rdx.works>
2022-07-14 11:10:49 +0100

    Added extra capability tests to AuthHandshakerTest

--- core/src/test/java/com/radixdlt/p2p/transport/handshake/AuthHandshakerTest.java
+++ core/src/test/java/com/radixdlt/p2p/transport/handshake/AuthHandshakerTest.java
@@ -64,20 +64,26 @@
 
 package com.radixdlt.p2p.transport.handshake;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import static com.radixdlt.utils.Bytes.bigIntegerToBytes;
+import static com.radixdlt.utils.Bytes.xor;
+import static org.junit.Assert.*;
 
-import com.radixdlt.crypto.ECKeyOps;
-import com.radixdlt.crypto.ECKeyPair;
+import com.google.common.hash.HashCode;
+import com.radixdlt.crypto.*;
 import com.radixdlt.p2p.capability.Capabilities;
 import com.radixdlt.p2p.capability.LedgerSyncCapability;
 import com.radixdlt.p2p.capability.RemotePeerCapability;
 import com.radixdlt.p2p.transport.handshake.AuthHandshakeResult.AuthHandshakeError;
 import com.radixdlt.p2p.transport.handshake.AuthHandshakeResult.AuthHandshakeSuccess;
 import com.radixdlt.serialization.DefaultSerialization;
+import com.radixdlt.serialization.DsonOutput;
 import com.radixdlt.serialization.Serialization;
 import io.netty.buffer.Unpooled;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
 import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.junit.Assert;
@@ -86,7 +92,6 @@ import org.junit.Test;
 public final class AuthHandshakerTest {
   private final Serialization serialization = DefaultSerialization.getInstance();
   private final SecureRandom secureRandom = new SecureRandom();
-
   private final Capabilities capabilities =
       new Capabilities(LedgerSyncCapability.Builder.asDefault().build());
 
@@ -201,5 +206,321 @@ public final class AuthHandshakerTest {
         handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
     assertTrue(handshaker2ResultPair.getSecond() instanceof AuthHandshakeError);
     assertArrayEquals(new byte[] {0x02}, handshaker2ResultPair.getFirst());
+  }
+
+  @Test
+  public void test_auth_handshake_fail_when_capability_count_gt_max_allowed() {
+    try {
+
+      var forkName = "fork1";
+      var networkId = (byte) 0x01;
+      final var nodeKey1 = ECKeyPair.generateNew();
+      final var nodeKey2 = ECKeyPair.generateNew();
+      var remotePeerCapabilities = new HashSet<RemotePeerCapability>();
+      for (var lc = 0; lc < Capabilities.MAX_NUMBER_OF_CAPABILITIES_ACCEPTED + 1; lc++) {
+        remotePeerCapabilities.add(new RemotePeerCapability("DUMMY_CAPABILITY" + lc, Map.of()));
+      }
+
+      final var handshaker1 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey1),
+              networkId,
+              forkName,
+              capabilities);
+      final var handshaker2 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey2),
+              networkId,
+              forkName,
+              capabilities);
+
+      var initMessage =
+          initiate(nodeKey2, handshaker1, remotePeerCapabilities, networkId, forkName);
+      var handshaker2ResultPair =
+          handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
+
+      assertTrue(
+          "Handshake should fail when number of capabilities exceed allowed maximum",
+          handshaker2ResultPair.getSecond() instanceof AuthHandshakeError);
+    } catch (Exception ex) {
+      fail(String.format("Exception %s", ex.getMessage()));
+    }
+  }
+
+  @Test
+  public void test_auth_handshake_ok_when_capability_has_config_values() {
+    try {
+      var forkName = "fork1";
+      var networkId = (byte) 0x01;
+      final var nodeKey1 = ECKeyPair.generateNew();
+      final var nodeKey2 = ECKeyPair.generateNew();
+      var remotePeerCapabilities =
+          Set.of(
+              new RemotePeerCapability(
+                  LedgerSyncCapability.NAME,
+                  createConfigMap(RemotePeerCapability.CONFIGURATION_MAP_MAX_SIZE)));
+
+      final var handshaker1 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey1),
+              networkId,
+              forkName,
+              capabilities);
+      final var handshaker2 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey2),
+              networkId,
+              forkName,
+              capabilities);
+
+      var initMessage =
+          initiate(nodeKey2, handshaker1, remotePeerCapabilities, networkId, forkName);
+      var handshaker2ResultPair =
+          handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
+      var handshaker2Result = (AuthHandshakeSuccess) handshaker2ResultPair.getSecond();
+      var capability =
+          handshaker2Result.remotePeerCapabilities().stream()
+              .filter(c -> c.getName().equals(LedgerSyncCapability.NAME))
+              .findFirst()
+              .get();
+
+      assertTrue(
+          String.format(
+              "Capability count incorrect. Expecting (%d), actual (%d)",
+              capability.getConfiguration().size(),
+              RemotePeerCapability.CONFIGURATION_MAP_MAX_SIZE),
+          capability.getConfiguration().size() == RemotePeerCapability.CONFIGURATION_MAP_MAX_SIZE);
+
+    } catch (Exception ex) {
+      fail(String.format("Exception %s", ex.getMessage()));
+    }
+  }
+
+  @Test
+  public void test_auth_handshake_fail_when_capability_config_count_gt_max_allowed() {
+    try {
+
+      var forkName = "fork1";
+      var networkId = (byte) 0x01;
+      final var nodeKey1 = ECKeyPair.generateNew();
+      final var nodeKey2 = ECKeyPair.generateNew();
+      var remotePeerCapabilities =
+          Set.of(
+              new RemotePeerCapability(
+                  LedgerSyncCapability.NAME,
+                  createConfigMap(RemotePeerCapability.CONFIGURATION_MAP_MAX_SIZE + 1)),
+              new RemotePeerCapability("DUMMY_CAPABILITY", Map.of()));
+
+      final var handshaker1 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey1),
+              networkId,
+              forkName,
+              capabilities);
+      final var handshaker2 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey2),
+              networkId,
+              forkName,
+              capabilities);
+
+      var initMessage =
+          initiate(nodeKey2, handshaker1, remotePeerCapabilities, networkId, forkName);
+      var handshaker2ResultPair =
+          handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
+
+      assertTrue(
+          "Handshake should fail when number of capability configurations exceeds allowed maximum",
+          handshaker2ResultPair.getSecond() instanceof AuthHandshakeError);
+    } catch (Exception ex) {
+      fail(String.format("Exception %s", ex.getMessage()));
+    }
+  }
+
+  @Test
+  public void test_auth_handshake_ok_when_capability_config_name_gt_max_allowed() {
+    try {
+
+      var forkName = "fork1";
+      var networkId = (byte) 0x01;
+      final var nodeKey1 = ECKeyPair.generateNew();
+      final var nodeKey2 = ECKeyPair.generateNew();
+      var remotePeerCapabilities =
+          Set.of(
+              new RemotePeerCapability(
+                  LedgerSyncCapability.NAME,
+                  Map.of(
+                      "N".repeat(RemotePeerCapability.CONFIGURATION_MAX_NAME_SIZE + 1), "value")));
+
+      final var handshaker1 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey1),
+              networkId,
+              forkName,
+              capabilities);
+      final var handshaker2 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey2),
+              networkId,
+              forkName,
+              capabilities);
+
+      var initMessage =
+          initiate(nodeKey2, handshaker1, remotePeerCapabilities, networkId, forkName);
+      var handshaker2ResultPair =
+          handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
+
+      assertTrue(
+          "Handshake should fail when a capability config name exceeds maximum allowed length",
+          handshaker2ResultPair.getSecond() instanceof AuthHandshakeError);
+    } catch (Exception ex) {
+      fail(String.format("Exception %s", ex.getMessage()));
+    }
+  }
+
+  @Test
+  public void test_auth_handshake_ok_when_capability_config_value_gt_max_allowed() {
+    try {
+
+      var forkName = "fork1";
+      var networkId = (byte) 0x01;
+      final var nodeKey1 = ECKeyPair.generateNew();
+      final var nodeKey2 = ECKeyPair.generateNew();
+      var remotePeerCapabilities =
+          Set.of(
+              new RemotePeerCapability(
+                  LedgerSyncCapability.NAME,
+                  Map.of(
+                      "configName",
+                      "V".repeat(RemotePeerCapability.CONFIGURATION_MAX_VALUE_SIZE + 1))));
+
+      final var handshaker1 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey1),
+              networkId,
+              forkName,
+              capabilities);
+      final var handshaker2 =
+          new AuthHandshaker(
+              serialization,
+              secureRandom,
+              ECKeyOps.fromKeyPair(nodeKey2),
+              networkId,
+              forkName,
+              capabilities);
+
+      var initMessage =
+          initiate(nodeKey2, handshaker1, remotePeerCapabilities, networkId, forkName);
+      var handshaker2ResultPair =
+          handshaker2.handleInitialMessage(Unpooled.wrappedBuffer(initMessage));
+
+      assertTrue(
+          "Handshake should fail when a capability config name exceeds maximum allowed length",
+          handshaker2ResultPair.getSecond() instanceof AuthHandshakeError);
+    } catch (Exception ex) {
+      fail(String.format("Exception %s", ex.getMessage()));
+    }
+  }
+
+  /*
+   The AuthHandshaker->initiate and createAuthInitiateMessage methods have been copied here and tweaked to allow test cases to create an auth init message
+   with a custom RemotePeerCapability set. The init message will be correctly signed, so the target handshaker handleInitialMessage method can validate
+   and decode / decrypt it.
+  */
+  private byte[] initiate(
+      ECKeyPair remoteKey,
+      AuthHandshaker authHandshaker,
+      Set<RemotePeerCapability> remotePeerCapabilities,
+      int networkId,
+      String forkName)
+      throws Exception {
+    var remotePubKey = remoteKey.getPublicKey();
+    final var message =
+        createAuthInitiateMessage(
+            remoteKey, authHandshaker, remotePeerCapabilities, networkId, forkName);
+    final var encoded = serialization.toDson(message, DsonOutput.Output.WIRE);
+    final var MIN_PADDING = 100;
+    final var MAX_PADDING = 300;
+    final var padding = new byte[secureRandom.nextInt(MAX_PADDING - MIN_PADDING) + MIN_PADDING];
+    secureRandom.nextBytes(padding);
+    final var padded = new byte[encoded.length + padding.length];
+    System.arraycopy(encoded, 0, padded, 0, encoded.length);
+    System.arraycopy(padding, 0, padded, encoded.length, padding.length);
+
+    final var encryptedSize = padded.length + ECIESCoder.OVERHEAD_SIZE;
+    final var sizePrefix = ByteBuffer.allocate(2).putShort((short) encryptedSize).array();
+    final var encryptedPayload = ECIESCoder.encrypt(remotePubKey.getEcPoint(), padded, sizePrefix);
+    final var packet = new byte[sizePrefix.length + encryptedPayload.length];
+    System.arraycopy(sizePrefix, 0, packet, 0, sizePrefix.length);
+    System.arraycopy(encryptedPayload, 0, packet, sizePrefix.length, encryptedPayload.length);
+
+    return packet;
+  }
+
+  private AuthInitiateMessage createAuthInitiateMessage(
+      ECKeyPair remoteKey,
+      AuthHandshaker authHandshaker,
+      Set<RemotePeerCapability> remotePeerCapabilities,
+      int networkId,
+      String forkName)
+      throws Exception {
+    Field AuthHandshakerField = AuthHandshaker.class.getDeclaredField("nonce");
+    AuthHandshakerField.setAccessible(true);
+    byte[] nonce = (byte[]) AuthHandshakerField.get(authHandshaker);
+
+    AuthHandshakerField = AuthHandshaker.class.getDeclaredField("ephemeralKey");
+    AuthHandshakerField.setAccessible(true);
+    ECKeyPair ephemeralKey = (ECKeyPair) AuthHandshakerField.get(authHandshaker);
+
+    var ecKeyOps = ECKeyOps.fromKeyPair(remoteKey);ß
+    var remotePubKey = remoteKey.getPublicKey();
+
+    final var sharedSecret = bigIntegerToBytes(ecKeyOps.ecdhAgreement(remotePubKey), 32);
+    final var messageToSign = xor(sharedSecret, nonce);
+    final var signature = ephemeralKey.sign(messageToSign);
+
+    // Instantiate with null capabilities.
+    var authInitMessage =
+        new AuthInitiateMessage(
+            signature,
+            HashCode.fromBytes(ecKeyOps.nodePubKey().getBytes()),
+            HashCode.fromBytes(nonce),
+            networkId,
+            forkName,
+            null);
+
+    // Set the capabilities using reflections. This will bypass the BaseHandshakeMessage class
+    // validation, which we want
+    // to do when sending capabilities that exceed the specified limits.
+    AuthHandshakerField = BaseHandshakeMessage.class.getDeclaredField("capabilities");
+    AuthHandshakerField.setAccessible(true);
+    AuthHandshakerField.set(authInitMessage, remotePeerCapabilities);
+    return authInitMessage;
+  }
+
+  private Map<String, String> createConfigMap(int numbOfEntries) {
+    var configMap = new HashMap<String, String>();
+    for (var lc = 0; lc < numbOfEntries; lc++) {
+      configMap.put("config" + lc, "value" + lc);
+    }
+    return configMap;
   }
 }
