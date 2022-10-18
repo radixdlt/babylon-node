dc2d69e69060dd30d7c19aff7f8b45da89882b34
Adam Mozes <adam.mozes@rdx.works>
2022-10-18 16:35:09 +0100

    chore: run cargo fix to fix unused imports
    Signed-off-by: Adam Mozes <adam.mozes@rdx.works>

--- core-rust/core-api-server/src/core_api/handlers/v0/transaction_submit.rs
+++ core-rust/core-api-server/src/core_api/handlers/v0/transaction_submit.rs
@@ -2,8 +2,6 @@ use crate::core_api::handlers::extract_unvalidated_transaction;
 use crate::core_api::*;
 
 use state_manager::jni::state_manager::ActualStateManager;
-
-use state_manager::mempool::transaction_rejection_cache::RejectionReason;
 use state_manager::MempoolAddError;
 
 pub(crate) async fn handle_v0_transaction_submit(
