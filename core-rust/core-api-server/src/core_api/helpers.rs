7da1f889a6b2b136698c652dd72029f3991b9232
Adam Mozes <adam.mozes@rdx.works>
2022-10-28 10:27:48 +0100

    refactor: make core_api_handler* functions a bit thinner
    Signed-off-by: Adam Mozes <adam.mozes@rdx.works>

--- core-rust/core-api-server/src/core_api/helpers.rs
+++ core-rust/core-api-server/src/core_api/helpers.rs
@@ -1,48 +1,39 @@
 use radix_engine::ledger::ReadableSubstateStore;
 use radix_engine::model::PersistedSubstate;
 use scrypto::engine::types::{GlobalAddress, SubstateId, SubstateOffset};
-use std::ops::DerefMut;
-
-use super::{Extension, Json};
 use state_manager::{jni::state_manager::ActualStateManager, query::StateManagerSubstateQueries};
 
-use super::*;
+use super::{CoreApiState, Extension, Json, RequestHandlingError};
 
 pub(crate) fn core_api_handler_empty_request<Response>(
-    state: Extension<CoreApiState>,
+    Extension(state): Extension<CoreApiState>,
     method: impl FnOnce(&mut ActualStateManager) -> Result<Response, RequestHandlingError>,
 ) -> Result<Json<Response>, RequestHandlingError> {
-    let state_manager_arc = state.0.state_manager;
-    let mut state_manager = state_manager_arc.write();
+    let mut state_manager = state.state_manager.write();
 
-    let response = method(state_manager.deref_mut())?;
-    Ok(Json(response))
+    method(&mut state_manager).map(Json)
 }
 
 #[tracing::instrument(skip_all)]
 pub(crate) fn core_api_handler<Request, Response>(
-    state: Extension<CoreApiState>,
+    Extension(state): Extension<CoreApiState>,
     Json(request_body): Json<Request>,
     method: impl FnOnce(&mut ActualStateManager, Request) -> Result<Response, RequestHandlingError>,
 ) -> Result<Json<Response>, RequestHandlingError> {
-    let state_manager_arc = state.0.state_manager;
-    let mut state_manager = state_manager_arc.write();
+    let mut state_manager = state.state_manager.write();
 
-    let response = method(state_manager.deref_mut(), request_body)?;
-    Ok(Json(response))
+    method(&mut state_manager, request_body).map(Json)
 }
 
 #[tracing::instrument(skip_all)]
 pub(crate) fn core_api_read_handler<Request, Response>(
-    state: Extension<CoreApiState>,
+    Extension(state): Extension<CoreApiState>,
     Json(request_body): Json<Request>,
     method: impl FnOnce(&ActualStateManager, Request) -> Result<Response, RequestHandlingError>,
 ) -> Result<Json<Response>, RequestHandlingError> {
-    let state_manager_arc = state.0.state_manager;
-    let state_manager = state_manager_arc.read();
+    let state_manager = state.state_manager.read();
 
-    let response = method(&state_manager, request_body)?;
-    Ok(Json(response))
+    method(&state_manager, request_body).map(Json)
 }
 
 #[tracing::instrument(skip_all)]
