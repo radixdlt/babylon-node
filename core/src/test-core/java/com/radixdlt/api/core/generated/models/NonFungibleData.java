20f3afd529a10f383fb3e4ca124fb4613e6c6b59
David Edey <david.edey@rdx.works>
2022-12-07 14:51:02 +0000

    fix: Don't throw mapping error with non-compliant non-fungible data

--- core/src/test-core/java/com/radixdlt/api/core/generated/models/NonFungibleData.java
+++ core/src/test-core/java/com/radixdlt/api/core/generated/models/NonFungibleData.java
@@ -29,20 +29,28 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 
 /**
- * Only present if the non fungible isn&#39;t deleted
+ * Only present if the non fungible isn&#39;t deleted. NOTE: Currently there is no schema for mutable/immutable data, and it&#39;s not even guaranteed to be SBOR-encoded. But from scrypto, it likely always will be. For now, immutable_data / mutable_data is optional, and only included if the data is valid SBOR. When the payload is validated, &#x60;immutable_data_raw_hex&#x60; / &#x60;mutable_data_raw_hex&#x60; will be removed. 
  */
-@ApiModel(description = "Only present if the non fungible isn't deleted")
+@ApiModel(description = "Only present if the non fungible isn't deleted. NOTE: Currently there is no schema for mutable/immutable data, and it's not even guaranteed to be SBOR-encoded. But from scrypto, it likely always will be. For now, immutable_data / mutable_data is optional, and only included if the data is valid SBOR. When the payload is validated, `immutable_data_raw_hex` / `mutable_data_raw_hex` will be removed. ")
 @JsonPropertyOrder({
   NonFungibleData.JSON_PROPERTY_IMMUTABLE_DATA,
-  NonFungibleData.JSON_PROPERTY_MUTABLE_DATA
+  NonFungibleData.JSON_PROPERTY_IMMUTABLE_DATA_RAW_HEX,
+  NonFungibleData.JSON_PROPERTY_MUTABLE_DATA,
+  NonFungibleData.JSON_PROPERTY_MUTABLE_DATA_RAW_HEX
 })
 @javax.annotation.processing.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class NonFungibleData {
   public static final String JSON_PROPERTY_IMMUTABLE_DATA = "immutable_data";
   private DataStruct immutableData;
 
+  public static final String JSON_PROPERTY_IMMUTABLE_DATA_RAW_HEX = "immutable_data_raw_hex";
+  private String immutableDataRawHex;
+
   public static final String JSON_PROPERTY_MUTABLE_DATA = "mutable_data";
   private DataStruct mutableData;
+
+  public static final String JSON_PROPERTY_MUTABLE_DATA_RAW_HEX = "mutable_data_raw_hex";
+  private String mutableDataRawHex;
 
   public NonFungibleData() { 
   }
@@ -56,10 +64,10 @@ public class NonFungibleData {
    * Get immutableData
    * @return immutableData
   **/
-  @javax.annotation.Nonnull
-  @ApiModelProperty(required = true, value = "")
+  @javax.annotation.Nullable
+  @ApiModelProperty(value = "")
   @JsonProperty(JSON_PROPERTY_IMMUTABLE_DATA)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
 
   public DataStruct getImmutableData() {
     return immutableData;
@@ -67,9 +75,35 @@ public class NonFungibleData {
 
 
   @JsonProperty(JSON_PROPERTY_IMMUTABLE_DATA)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public void setImmutableData(DataStruct immutableData) {
     this.immutableData = immutableData;
+  }
+
+
+  public NonFungibleData immutableDataRawHex(String immutableDataRawHex) {
+    this.immutableDataRawHex = immutableDataRawHex;
+    return this;
+  }
+
+   /**
+   * The hex-encoded raw bytes of the immutable data of the NF. 
+   * @return immutableDataRawHex
+  **/
+  @javax.annotation.Nonnull
+  @ApiModelProperty(required = true, value = "The hex-encoded raw bytes of the immutable data of the NF. ")
+  @JsonProperty(JSON_PROPERTY_IMMUTABLE_DATA_RAW_HEX)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public String getImmutableDataRawHex() {
+    return immutableDataRawHex;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_IMMUTABLE_DATA_RAW_HEX)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public void setImmutableDataRawHex(String immutableDataRawHex) {
+    this.immutableDataRawHex = immutableDataRawHex;
   }
 
 
@@ -82,10 +116,10 @@ public class NonFungibleData {
    * Get mutableData
    * @return mutableData
   **/
-  @javax.annotation.Nonnull
-  @ApiModelProperty(required = true, value = "")
+  @javax.annotation.Nullable
+  @ApiModelProperty(value = "")
   @JsonProperty(JSON_PROPERTY_MUTABLE_DATA)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
 
   public DataStruct getMutableData() {
     return mutableData;
@@ -93,9 +127,35 @@ public class NonFungibleData {
 
 
   @JsonProperty(JSON_PROPERTY_MUTABLE_DATA)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public void setMutableData(DataStruct mutableData) {
     this.mutableData = mutableData;
+  }
+
+
+  public NonFungibleData mutableDataRawHex(String mutableDataRawHex) {
+    this.mutableDataRawHex = mutableDataRawHex;
+    return this;
+  }
+
+   /**
+   * The hex-encoded raw bytes of the mutadata data of the NF. 
+   * @return mutableDataRawHex
+  **/
+  @javax.annotation.Nonnull
+  @ApiModelProperty(required = true, value = "The hex-encoded raw bytes of the mutadata data of the NF. ")
+  @JsonProperty(JSON_PROPERTY_MUTABLE_DATA_RAW_HEX)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public String getMutableDataRawHex() {
+    return mutableDataRawHex;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_MUTABLE_DATA_RAW_HEX)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public void setMutableDataRawHex(String mutableDataRawHex) {
+    this.mutableDataRawHex = mutableDataRawHex;
   }
 
 
@@ -112,12 +172,14 @@ public class NonFungibleData {
     }
     NonFungibleData nonFungibleData = (NonFungibleData) o;
     return Objects.equals(this.immutableData, nonFungibleData.immutableData) &&
-        Objects.equals(this.mutableData, nonFungibleData.mutableData);
+        Objects.equals(this.immutableDataRawHex, nonFungibleData.immutableDataRawHex) &&
+        Objects.equals(this.mutableData, nonFungibleData.mutableData) &&
+        Objects.equals(this.mutableDataRawHex, nonFungibleData.mutableDataRawHex);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(immutableData, mutableData);
+    return Objects.hash(immutableData, immutableDataRawHex, mutableData, mutableDataRawHex);
   }
 
   @Override
@@ -125,7 +187,9 @@ public class NonFungibleData {
     StringBuilder sb = new StringBuilder();
     sb.append("class NonFungibleData {\n");
     sb.append("    immutableData: ").append(toIndentedString(immutableData)).append("\n");
+    sb.append("    immutableDataRawHex: ").append(toIndentedString(immutableDataRawHex)).append("\n");
     sb.append("    mutableData: ").append(toIndentedString(mutableData)).append("\n");
+    sb.append("    mutableDataRawHex: ").append(toIndentedString(mutableDataRawHex)).append("\n");
     sb.append("}");
     return sb.toString();
   }
