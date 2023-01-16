4b810b392d837b6a058c30ff937cbf08a89882b0
David Edey <david.edey@rdx.works>
2023-01-16 02:00:20 +0000

    feat: Vastly improved Core API clarity for rejections (status and submit)

--- core-rust/core-api-server/src/core_api/handlers/transaction_submit.rs
+++ core-rust/core-api-server/src/core_api/handlers/transaction_submit.rs
@@ -1,14 +1,18 @@
 use crate::core_api::*;
 
+use hyper::StatusCode;
+use models::transaction_submit_error_details::TransactionSubmitErrorDetails;
 use state_manager::transaction::UserTransactionValidator;
 use state_manager::{MempoolAddError, MempoolAddSource};
 use transaction::model::NotarizedTransaction;
 
-#[tracing::instrument(level = "debug", skip(state), err(Debug))]
+impl ErrorDetails for TransactionSubmitErrorDetails {}
+
+// #[tracing::instrument(level = "debug", skip(state), err(Debug))]
 pub(crate) async fn handle_transaction_submit(
     Extension(state): Extension<CoreApiState>,
     Json(request): Json<models::TransactionSubmitRequest>,
-) -> Result<Json<models::TransactionSubmitResponse>, RequestHandlingError> {
+) -> Result<Json<models::TransactionSubmitResponse>, ResponseError<TransactionSubmitErrorDetails>> {
     let mut state_manager = state.state_manager.write();
 
     assert_matching_network(&request.network, &state_manager.network)?;
@@ -22,13 +26,41 @@ pub(crate) async fn handle_transaction_submit(
     match result {
         Ok(_) => Ok(models::TransactionSubmitResponse::new(false)),
         Err(MempoolAddError::Duplicate) => Ok(models::TransactionSubmitResponse::new(true)),
-        Err(MempoolAddError::Full {
-            current_size: _,
-            max_size: _,
-        }) => Err(client_error("Mempool is full")),
-        Err(MempoolAddError::Rejected(rejection)) => {
-            Err(client_error(format!("Rejected: {}", rejection.reason)))
-        }
+        Err(MempoolAddError::Full { max_size, .. }) => Err(detailed_error(
+            StatusCode::BAD_REQUEST,
+            "Mempool is full",
+            TransactionSubmitErrorDetails::TransactionSubmitMempoolFullErrorDetails {
+                mempool_capacity: max_size as i32,
+            },
+        )),
+        Err(MempoolAddError::Rejected(rejection)) => Err(detailed_error(
+            StatusCode::BAD_REQUEST,
+            "Transaction was rejected",
+            TransactionSubmitErrorDetails::TransactionSubmitRejectedErrorDetails {
+                error_message: format!("{}", rejection.reason),
+                is_fresh: !rejection.was_cached,
+                is_payload_rejection_permanent: rejection.is_permanent_for_payload(),
+                is_intent_rejection_permanent: rejection.is_permanent_for_intent(),
+                is_rejected_because_intent_already_committed: rejection
+                    .is_rejected_because_intent_already_committed(),
+                recalculation_due: match rejection.recalculation_due {
+                    state_manager::RecalculationDue::Never => None,
+                    state_manager::RecalculationDue::From(time) => {
+                        Some(to_api_unix_timestamp_ms(time)?)
+                    }
+                    state_manager::RecalculationDue::Whenever => {
+                        Some(to_api_unix_timestamp_ms(std::time::SystemTime::now())?)
+                    }
+                },
+                invalid_from_epoch: if rejection.is_permanent_for_payload() {
+                    None
+                } else {
+                    // If it's a temporary rejection, then the transaction must have passed validation...
+                    // So the epoch quoted below must be in a valid range to pass the `to_api_epoch` mapping
+                    Some(to_api_epoch(rejection.invalid_from_epoch)?)
+                },
+            },
+        )),
     }
     .map(Json)
 }
