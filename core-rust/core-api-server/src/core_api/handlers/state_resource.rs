eac7ab30d861fda8642e43eb1c93af9a7873f783
David Edey <david.edey@rdx.works>
2023-03-21 18:21:57 +0000

    fix: Fix state/resource for non fungibles

--- core-rust/core-api-server/src/core_api/handlers/state_resource.rs
+++ core-rust/core-api-server/src/core_api/handlers/state_resource.rs
@@ -1,6 +1,7 @@
 use crate::core_api::*;
+use radix_engine::blueprints::resource::{FungibleResourceManagerSubstate, NonFungibleResourceManagerSubstate};
 use radix_engine::system::node_substates::PersistedSubstate;
-use radix_engine::types::{ResourceManagerOffset, SubstateOffset};
+use radix_engine::types::{ResourceManagerOffset, SubstateOffset, ResourceAddress};
 use radix_engine_interface::api::types::{AccessRulesOffset, NodeModuleId, RENodeId};
 
 use state_manager::jni::state_manager::ActualStateManager;
@@ -10,6 +11,11 @@ pub(crate) async fn handle_state_resource(
     request: Json<models::StateResourceRequest>,
 ) -> Result<Json<models::StateResourceResponse>, ResponseError<()>> {
     core_api_read_handler(state, request, handle_state_resource_internal)
+}
+
+enum ManagerByType {
+    Fungible(FungibleResourceManagerSubstate),
+    NonFungible(NonFungibleResourceManagerSubstate),
 }
 
 fn handle_state_resource_internal(
@@ -22,19 +28,35 @@ fn handle_state_resource_internal(
     let resource_address = extract_resource_address(&extraction_context, &request.resource_address)
         .map_err(|err| err.into_response_error("resource_address"))?;
 
-    let resource_manager = {
-        let substate_offset =
-            SubstateOffset::ResourceManager(ResourceManagerOffset::ResourceManager);
-        let loaded_substate = read_mandatory_substate_or_server_error(
-            state_manager,
-            RENodeId::GlobalObject(resource_address.into()),
-            NodeModuleId::SELF,
-            &substate_offset,
-        )?;
-        let PersistedSubstate::ResourceManager(substate) = loaded_substate else {
-            return Err(wrong_substate_type(substate_offset));
-        };
-        substate
+    let manager = match &resource_address {
+        ResourceAddress::Fungible(_) => {
+            let substate_offset =
+                SubstateOffset::ResourceManager(ResourceManagerOffset::ResourceManager);
+            let loaded_substate = read_mandatory_substate_or_server_error(
+                state_manager,
+                RENodeId::GlobalObject(resource_address.into()),
+                NodeModuleId::SELF,
+                &substate_offset,
+            )?;
+            let PersistedSubstate::ResourceManager(substate) = loaded_substate else {
+                return Err(wrong_substate_type(substate_offset));
+            };
+            ManagerByType::Fungible(substate)
+        },
+        ResourceAddress::NonFungible(_) => {
+            let substate_offset =
+                SubstateOffset::ResourceManager(ResourceManagerOffset::ResourceManager);
+            let loaded_substate = read_mandatory_substate_or_server_error(
+                state_manager,
+                RENodeId::GlobalObject(resource_address.into()),
+                NodeModuleId::SELF,
+                &substate_offset,
+            )?;
+            let PersistedSubstate::NonFungibleResourceManager(substate) = loaded_substate else {
+                return Err(wrong_substate_type(substate_offset));
+            };
+            ManagerByType::NonFungible(substate)
+        },
     };
     let access_rules = {
         let substate_offset = SubstateOffset::AccessRules(AccessRulesOffset::AccessRules);
@@ -49,6 +71,7 @@ fn handle_state_resource_internal(
         };
         substate
     };
+
     let vault_access_rules = {
         let substate_offset = SubstateOffset::AccessRules(AccessRulesOffset::AccessRules);
         let loaded_substate = read_mandatory_substate_or_server_error(
@@ -66,10 +89,14 @@ fn handle_state_resource_internal(
     let mapping_context = MappingContext::new(&state_manager.network);
 
     Ok(models::StateResourceResponse {
-        manager: Some(to_api_fungible_resource_manager_substate(
-            &mapping_context,
-            &resource_manager,
-        )?),
+        manager: Some(match &manager {
+            ManagerByType::Fungible(manager) => {
+                to_api_fungible_resource_manager_substate(&mapping_context, manager)?
+            }
+            ManagerByType::NonFungible(manager) => {
+                to_api_non_fungible_resource_manager_substate(&mapping_context, manager)?
+            }
+        }),
         access_rules: Some(to_api_access_rules_chain_substate(
             &mapping_context,
             &access_rules,
@@ -79,4 +106,5 @@ fn handle_state_resource_internal(
             &vault_access_rules,
         )?),
     })
+
 }
