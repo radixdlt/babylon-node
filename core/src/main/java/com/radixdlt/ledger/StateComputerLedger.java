38f22a69986b66508daeadf20ccf888ccb6768fa
Jakub Krawczyk <jakub.krawczyk@rdx.works>
2023-04-19 15:43:52 +0200

    Postponing a TODO for later.

--- core/src/main/java/com/radixdlt/ledger/StateComputerLedger.java
+++ core/src/main/java/com/radixdlt/ledger/StateComputerLedger.java
@@ -154,7 +154,7 @@ public final class StateComputerLedger implements Ledger, ProposalGenerator {
   private final Metrics metrics;
   private final LedgerAccumulator accumulator;
   private final LedgerAccumulatorVerifier verifier;
-  // TODO(resolve during review): Is it still needed? Was it ever needed?
+  // TODO: This does not seem needed - remove in a narrow-focus PR.
   private final Object lock = new Object();
   private final TimeSupplier timeSupplier;
 
