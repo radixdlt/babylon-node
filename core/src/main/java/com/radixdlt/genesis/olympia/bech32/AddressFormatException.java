bc48e6c3478e8b08693470d1504b2e1b76a59c0f
Lukasz Gasior <lukasz.gasior@rdx.works>
2023-05-18 12:47:18 +0200

    Add basic auth - wip

--- core/src/main/java/com/radixdlt/genesis/olympia/bech32/AddressFormatException.java
+++ core/src/main/java/com/radixdlt/genesis/olympia/bech32/AddressFormatException.java
@@ -62,79 +62,40 @@
  * permissions under this License.
  */
 
-package com.radixdlt.crypto;
+package com.radixdlt.genesis.olympia.bech32;
 
-import org.bouncycastle.crypto.DataLengthException;
-import org.bouncycastle.crypto.DerivationFunction;
-import org.bouncycastle.crypto.DerivationParameters;
-import org.bouncycastle.crypto.Digest;
-import org.bouncycastle.crypto.params.MGFParameters;
-
-/**
- * This class is borrowed from spongycastle project The only change made is addition of
- * 'counterStart' parameter to conform to Crypto++ capabilities
- */
-public final class MGF1BytesGeneratorExt implements DerivationFunction {
-  private Digest digest;
-  private byte[] seed;
-  private int hLen;
-  private int counterStart;
-
-  public MGF1BytesGeneratorExt(Digest digest, int counterStart) {
-    this.digest = digest;
-    this.hLen = digest.getDigestSize();
-    this.counterStart = counterStart;
+@SuppressWarnings("serial")
+public class AddressFormatException extends IllegalArgumentException {
+  public AddressFormatException(String message) {
+    super(message);
   }
 
-  public void init(DerivationParameters param) {
-    if (!(param instanceof MGFParameters)) {
-      throw new IllegalArgumentException("MGF parameters required for MGF1Generator");
-    } else {
-      this.seed = ((MGFParameters) param).getSeed();
+  public static class InvalidCharacter extends AddressFormatException {
+    public final char character;
+    public final int position;
+
+    public InvalidCharacter(char character, int position) {
+      super("Invalid character '" + character + "' at position " + position);
+      this.character = character;
+      this.position = position;
     }
   }
 
-  public Digest getDigest() {
-    return this.digest;
+  public static class InvalidDataLength extends AddressFormatException {
+    public InvalidDataLength(String message) {
+      super(message);
+    }
   }
 
-  private void iToOsp(int i, byte[] sp) {
-    sp[0] = (byte) (i >>> 24);
-    sp[1] = (byte) (i >>> 16);
-    sp[2] = (byte) (i >>> 8);
-    sp[3] = (byte) (i >>> 0);
+  public static class InvalidChecksum extends AddressFormatException {
+    public InvalidChecksum() {
+      super("Checksum does not validate");
+    }
   }
 
-  public int generateBytes(byte[] out, int outOff, int len)
-      throws DataLengthException, IllegalArgumentException {
-    if (out.length - len < outOff) {
-      throw new DataLengthException("output buffer too small");
-    } else {
-      final var hashBuf = new byte[this.hLen];
-      final var c = new byte[4];
-      int counter = 0;
-      int hashCounter = counterStart;
-      this.digest.reset();
-      if (len > this.hLen) {
-        do {
-          this.iToOsp(hashCounter++, c);
-          this.digest.update(this.seed, 0, this.seed.length);
-          this.digest.update(c, 0, c.length);
-          this.digest.doFinal(hashBuf, 0);
-          System.arraycopy(hashBuf, 0, out, outOff + counter * this.hLen, this.hLen);
-          ++counter;
-        } while (counter < len / this.hLen);
-      }
-
-      if (counter * this.hLen < len) {
-        this.iToOsp(hashCounter, c);
-        this.digest.update(this.seed, 0, this.seed.length);
-        this.digest.update(c, 0, c.length);
-        this.digest.doFinal(hashBuf, 0);
-        System.arraycopy(hashBuf, 0, out, outOff + counter * this.hLen, len - counter * this.hLen);
-      }
-
-      return len;
+  public static class InvalidPrefix extends AddressFormatException {
+    public InvalidPrefix(String message) {
+      super(message);
     }
   }
 }
