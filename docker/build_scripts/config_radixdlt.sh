6b858551760bc80a0b54632a664bae7a5b01f51c
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-08-11 16:20:02 -0300

    add the option to use a different user and group id to start

--- docker/build_scripts/config_radixdlt.sh
+++ docker/build_scripts/config_radixdlt.sh
@@ -7,9 +7,7 @@ if test -f "$TEST_CONFIGS"; then
     echo >> default.config.envsubst
     cat $TEST_CONFIGS >> default.config.envsubst
 fi
-
 env | sort
-
 envsubst <"${RADIXDLT_HOME:?}"/default.config.envsubst >"${RADIXDLT_HOME:?}"/default.config
 
 # This command changes the process from executing this script to executing the argument to the script,
@@ -19,4 +17,10 @@ envsubst <"${RADIXDLT_HOME:?}"/default.config.envsubst >"${RADIXDLT_HOME:?}"/def
 # - The exec command replaces the shell with the given command without creating a new process
 # - The setuidgid command runs another program under a specified account's uid and gid
 # - And the $* command is a special variable that contains all the positional parameters 
+if [[ -z "${LOCAL_USER_ID}" ]]; then
+    echo "Running with default uid and gid..."
+else
+    usermod -u $LOCAL_USER_ID radixdlt    
+    groupmod -g $LOCAL_USER_ID radixdlt  
+fi
 exec setuidgid radixdlt $*
