332d884d42f3780f0517fee438238fbe4ab94ad8
Lukasz Gasior <lukasz.gasior@rdx.works>
2023-10-03 18:28:24 +0200

    Use exponent of 1 for the second timeout ocurrence

--- core/src/main/java/com/radixdlt/consensus/liveness/Pacemaker.java
+++ core/src/main/java/com/radixdlt/consensus/liveness/Pacemaker.java
@@ -463,7 +463,7 @@ public final class Pacemaker implements BFTEventProcessorAtCurrentRound {
   }
 
   private void rescheduleTimeout(ScheduledLocalTimeout scheduledTimeout) {
-    final var timeout = timeoutCalculator.calculateTimeoutMs(scheduledTimeout.count());
+    final var timeout = timeoutCalculator.calculateTimeoutMs(scheduledTimeout.count() + 1);
     final var nextTimeout = scheduledTimeout.nextRetry(timeout);
     this.scheduledLocalTimeoutDispatcher.dispatch(nextTimeout, timeout);
   }
