21c2ea569a7872704a6b49c8789052db516ab03f
David Edey <david.edey@rdx.works>
2024-01-24 16:18:41 +0000

    feature: Properly configure protocol configs

--- core-rust/state-manager/src/protocol/protocol_configs/mainnet_protocol_config.rs
+++ core-rust/state-manager/src/protocol/protocol_configs/mainnet_protocol_config.rs
@@ -4,17 +4,26 @@ use crate::protocol::*;
 use ProtocolUpdateEnactmentCondition::*;
 
 pub fn mainnet_protocol_config() -> ProtocolConfig {
+    // See config_printer.rs
     ProtocolConfig::new_with_triggers(hashmap! {
         ANEMONE_PROTOCOL_VERSION => EnactAtStartOfEpochIfValidatorsReady {
-            // TODO(anemone): update the epoch bounds and thresholds
-            lower_bound_inclusive: Epoch::of(10000),
-            upper_bound_exclusive: Epoch::of(20000),
+            // =================================================================
+            // PROTOCOL_VERSION: "anemone"
+            // READINESS_SIGNAL: "27ee917cdbf8a426000000000anemone"
+            // =================================================================
+            // The below estimates are based off:
+            // - Calculating relative to epoch 66516
+            // - Using that epoch 66516 started at 2024-01-24T14:05:57.229Z
+            // - Assuming epoch length will be 5 mins, 0 secs
+            // =================================================================
+            lower_bound_inclusive: Epoch::of(70019), // estimated: 2024-02-05T18:00:57.229Z
+            upper_bound_exclusive: Epoch::of(74051), // estimated: 2024-02-19T18:00:57.229Z
             readiness_thresholds: vec![
                 SignalledReadinessThreshold {
-                    required_ratio_of_stake_supported: dec!("0.80"),
-                    required_consecutive_completed_epochs_of_support: 10,
-                }
+                    required_ratio_of_stake_supported: dec!(0.8),
+                    required_consecutive_completed_epochs_of_support: 1152, // estimated: 4 days, 0 hours, 0 mins, 0 secs
+                },
             ],
-        }
+        },
     })
 }
