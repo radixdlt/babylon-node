d9625753649ace6d4f192ad3b1243b213de68d21
Jakub Krawczyk <jakub.krawczyk@rdx.works>
2024-05-10 12:10:50 +0200

    Make Dumunet automatically enact all protocol updates, for load-testing purposes.

--- core-rust/state-manager/src/protocol/protocol_configs/dumunet_protocol_config.rs
+++ core-rust/state-manager/src/protocol/protocol_configs/dumunet_protocol_config.rs
@@ -5,29 +5,7 @@ use ProtocolUpdateEnactmentCondition::*;
 
 pub fn dumunet_protocol_config() -> ProtocolConfig {
     ProtocolConfig::new_with_triggers(hashmap! {
-        ANEMONE_PROTOCOL_VERSION => EnactAtStartOfEpochIfValidatorsReady {
-            // =================================================================
-            // PROTOCOL_VERSION: "anemone"
-            // READINESS_SIGNAL: "811c31d2bc6a2631000000000anemone"
-            // =================================================================
-            lower_bound_inclusive: Epoch::of(1),
-            upper_bound_exclusive: Epoch::of(1000000),
-            readiness_thresholds: vec![SignalledReadinessThreshold {
-                required_ratio_of_stake_supported: dec!("0.80"),
-                required_consecutive_completed_epochs_of_support: 10,
-            }],
-        },
-        BOTTLENOSE_PROTOCOL_VERSION => EnactAtStartOfEpochIfValidatorsReady {
-            // =================================================================
-            // PROTOCOL_VERSION: "bottlenose"
-            // READINESS_SIGNAL: "35701a6147bfd870000000bottlenose"
-            // =================================================================
-            lower_bound_inclusive: Epoch::of(1),
-            upper_bound_exclusive: Epoch::of(1000000),
-            readiness_thresholds: vec![SignalledReadinessThreshold {
-                required_ratio_of_stake_supported: dec!("0.80"),
-                required_consecutive_completed_epochs_of_support: 10,
-            }],
-        }
+        ANEMONE_PROTOCOL_VERSION => EnactAtStartOfEpochUnconditionally(Epoch::of(3)),
+        BOTTLENOSE_PROTOCOL_VERSION => EnactAtStartOfEpochUnconditionally(Epoch::of(4))
     })
 }
