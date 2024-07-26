cf578ac44314627bcb2fdab251e4ed3445e742e4
Joshua Primero <josh.primero@rdx.works>
2024-07-26 12:04:53 -0500

    Add separate node-radix-engine crate

--- core-rust/node-radix-engine/src/lib.rs
+++ core-rust/node-radix-engine/src/lib.rs
@@ -62,40 +62,44 @@
  * permissions under this License.
  */
 
-extern crate core;
-
-mod accumulator_tree;
-mod commit_bundle;
-mod committer;
-pub mod jni;
-mod limits;
-pub mod mempool;
-pub mod metrics;
-pub mod protocol;
-pub mod query;
-mod receipt;
-mod staging;
-mod state_manager;
-pub mod store;
-pub mod system_commits;
-mod system_executor;
-pub mod transaction;
-mod types;
-
-#[cfg(test)]
-mod test;
-
-pub use crate::committer::*;
-pub use crate::mempool::*;
-pub use crate::metrics::*;
-pub use crate::pending_transaction_result_cache::*;
-pub use crate::receipt::*;
-pub use crate::staging::*;
-pub use crate::state_manager::*;
-pub use crate::store::*;
-pub use crate::system_executor::*;
-pub use crate::types::*;
-
 pub mod engine_prelude {
-    pub use node_radix_engine::engine_prelude::*;
+    pub use radix_common::prelude::*;
+
+    pub use radix_engine::errors::*;
+    pub use radix_engine::system::bootstrap::*;
+    #[cfg(feature = "db_checker")]
+    pub use radix_engine::system::checkers::*;
+    pub use radix_engine::system::system_db_reader::*;
+    pub use radix_engine::system::system_substates::*;
+    pub use radix_engine::transaction::*;
+    pub use radix_engine::updates::*;
+    pub use radix_engine::vm::*;
+    pub use radix_engine::*;
+
+    pub use radix_engine_interface::blueprints::account::*;
+    pub use radix_engine_interface::blueprints::transaction_processor::*;
+    pub use radix_engine_interface::prelude::*;
+
+    pub use radix_substate_store_impls::state_tree::tree_store::*;
+    pub use radix_substate_store_impls::state_tree::*;
+    pub use radix_substate_store_impls::*;
+    pub use radix_substate_store_interface::db_key_mapper::*;
+    pub use radix_substate_store_interface::interface::*;
+    pub use radix_substate_store_queries::query::*;
+    pub use radix_substate_store_queries::typed_substate_layout::*;
+
+    pub use radix_transaction_scenarios::scenario::*;
+
+    pub use radix_transactions::builder::*;
+    pub use radix_transactions::errors::*;
+    pub use radix_transactions::manifest::*;
+    pub use radix_transactions::model::*;
+    pub use radix_transactions::prelude::*;
+    pub use radix_transactions::validation::*;
+    pub use radix_transactions::*;
+
+    // Note: plain `pub use radix_engine::track::*` would clash with the top-level `utils::prelude`
+    // (because it contains a private module of the same name)
+    pub use radix_engine::track::interface::*;
+    pub use radix_engine::track::state_updates::*;
 }
