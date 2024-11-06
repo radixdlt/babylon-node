07045b175ae73296a6d86465ab6c331f79c8af1d
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-11-06 18:33:22 +0100

    Cleanup

--- core-rust/mesh-api-server/src/mesh_api/conversions/mod.rs
+++ core-rust/mesh-api-server/src/mesh_api/conversions/mod.rs
@@ -1,4 +1,5 @@
 mod addressing;
+mod block;
 mod common;
 mod context;
 mod errors;
@@ -6,6 +7,7 @@ mod hashes;
 mod numerics;
 
 pub use addressing::*;
+pub use block::*;
 pub use common::*;
 pub use context::*;
 pub use errors::*;
