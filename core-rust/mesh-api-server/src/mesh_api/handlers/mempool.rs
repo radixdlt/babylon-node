338e6ce13fdfe36c6ae2b287c98372573dd5c90f
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2024-11-11 17:54:07 +0100

    Implement mempool endpoint

--- core-rust/mesh-api-server/src/mesh_api/handlers/mempool.rs
+++ core-rust/mesh-api-server/src/mesh_api/handlers/mempool.rs
@@ -6,8 +6,17 @@ pub(crate) async fn handle_mempool(
 ) -> Result<Json<models::MempoolResponse>, ResponseError> {
     assert_matching_network(&request.network_identifier, &state.network)?;
 
-    // let database = state.state_manager.database.snapshot();
-    // let mapping_context = MappingContext::new(&state.network);
+    let mapping_context = MappingContext::new(&state.network);
+    let mempool = state.state_manager.mempool.read();
 
-    Ok(Json(models::MempoolResponse::new(vec![])))
+    Ok(Json(models::MempoolResponse::new(
+        mempool
+            .all_hashes_iter()
+            .map(|(intent_hash, _)| {
+                Ok(models::TransactionIdentifier {
+                    hash: to_api_transaction_hash_bech32m(&mapping_context, intent_hash)?,
+                })
+            })
+            .collect::<Result<Vec<_>, MappingError>>()?,
+    )))
 }
